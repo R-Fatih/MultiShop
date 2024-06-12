72caffd853481cc9cfad0096c1030c84aebad15e
RFatih <39864827+R-Fatih@users.noreply.github.com>
2024-06-13 01:19:07 +0300

    up

---
+++ RapidApi/MultiShop.RapidApiWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApiWebUI.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MultiShop.RapidApiWebUI.Controllers
+{
+	public class DefaultController : Controller
+	{
+		public async Task< IActionResult> WeatherDetail()
+		{
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://the-weather-api.p.rapidapi.com/api/weather/ankara"),
+				Headers =
+	{
+		{ "x-rapidapi-key", "b92489cde0msh89003454a330f43p17ffbejsn526f18837291" },
+		{ "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
+	},
+			};
+			
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var weather = JsonConvert.DeserializeObject<WeatherViewModel.Rootobject>(body);
+				ViewBag.temp=weather.data.temp;
+				return View(weather);
+			}
+
+			return View();
+		}
+	}
+}
--- Services/MultiShop.RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessagesController.cs
+++ Services/MultiShop.RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 namespace MultiShop.RabbitMQMessageApi.Controllers
@@ -21,15 +22,47 @@ namespace MultiShop.RabbitMQMessageApi.Controllers
 			var connection=connectionFactory.CreateConnection();
 			var channel=connection.CreateModel();
 
-			channel.QueueDeclare(queue:"message-queue",durable:false,exclusive:false,autoDelete:false,arguments:null);
+			channel.QueueDeclare(queue:"message-queue2",durable:false,exclusive:false,autoDelete:false,arguments:null);
 
-			var messageContent="Hello RabbitMQ";
+			var messageContent="Hello RabbitMQ 3";
 
 			var byteMessageContent=Encoding.UTF8.GetBytes(messageContent);
 
-			channel.BasicPublish(exchange: "", routingKey: "message-queue", basicProperties: null, byteMessageContent);
+			channel.BasicPublish(exchange: "", routingKey: "message-queue2", basicProperties: null, byteMessageContent);
 
 			return Ok("Message in queue.");
+		}
+		private static string _message;
+		[HttpGet]
+		public IActionResult GetMessage()
+		{
+			var connectionFactory=new ConnectionFactory()
+			{
+			HostName="localhost",
+			UserName="guest",
+			Password="guest"
+
+			};
+			var connection=connectionFactory.CreateConnection();
+			var channel=connection.CreateModel();
+
+
+			var consumer= new EventingBasicConsumer(channel);
+			consumer.Received += (model, ea) =>
+			{
+				var byteMessage = ea.Body.ToArray();
+				_message = Encoding.UTF8.GetString(byteMessage);
+
+			};
+			var result=channel.BasicConsume(queue:"message-queue2",autoAck:true,consumer:consumer);
+
+			if(string.IsNullOrEmpty(_message))
+			{
+				return NoContent();
+			}
+
+
+			return Ok(_message);
 		}
 	}
 }
